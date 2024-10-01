67f3ad20da89840b4726c8216897b902b723fa7b
Aarju2308 <Aarjupatel12347@gmail.com>
2024-10-01 16:33:49 +0530

    Fixed token interceptor

--- src/app/services/token-interceptor/token.interceptor.ts
+++ src/app/services/token-interceptor/token.interceptor.ts
@@ -5,8 +5,19 @@ import { TokenService } from '../token/token.service';
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
 
   const tokenServ = inject(TokenService);
-  // console.log(tokenServ.getAuthentication())
-  if (!req.url.includes("public") && !req.url.includes("auth")) {
+  console.log('Request URL:', req.url);
+  let normalizedUrl = req.url;
+  if (normalizedUrl.startsWith('//')) {
+    normalizedUrl = normalizedUrl.replace(/^\/\/[^\/]+/, ''); // Handle URLs starting with `//`
+  } else {
+    normalizedUrl = normalizedUrl.replace(/^https?:\/\/[^\/]+/, ''); // Remove http/https and domain
+  }
+  
+  // Check if the normalized URL contains "public" or "auth"
+  console.log(!normalizedUrl.includes("public") && !normalizedUrl.includes("auth"));
+  console.log('Normalized URL:', normalizedUrl);
+
+if (!normalizedUrl.includes("public") && !normalizedUrl.includes("auth")) {
     tokenServ.isAuthenticated.subscribe({
       next : (value)=> {
           if (value) {
@@ -19,6 +30,7 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
       },
     })
   }
+  console.log(req)
   return next(req);
 
 };
